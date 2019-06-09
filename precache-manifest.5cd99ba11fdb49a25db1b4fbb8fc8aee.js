70995c7ddc555951170c6e7983d6a41637601545
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-09 21:06:32 +0000

    Deploy sc.wrrr.dev to github.com/kaaboaye/smart-curtains-release.git:gh-pages

---
+++ precache-manifest.5cd99ba11fdb49a25db1b4fbb8fc8aee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0dfdd8246ca95cd04ae855bd055cb38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "564e60a8de380003c517",
+    "url": "/static/css/2.793a0409.chunk.css"
+  },
+  {
+    "revision": "1dc1d0daea0acf0231bc",
+    "url": "/static/css/main.f6e1149c.chunk.css"
+  },
+  {
+    "revision": "564e60a8de380003c517",
+    "url": "/static/js/2.5f4e652d.chunk.js"
+  },
+  {
+    "revision": "1dc1d0daea0acf0231bc",
+    "url": "/static/js/main.27109f32.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3280ca9c39eecdd0b83120cf9c4f1d09",
+    "url": "/static/media/schedule.3280ca9c.svg"
+  },
+  {
+    "revision": "c9afe93310f923a5c80c9d609ef6a028",
+    "url": "/static/media/sunny.c9afe933.svg"
+  }
+]);
